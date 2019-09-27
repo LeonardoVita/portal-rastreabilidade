7acf7ba9fb3d485509294b03d399bf1565252038
leonardo.vita <leonaro.vita0606@gmail.com>
2019-09-27 16:21:57 -0300

    projeto padrão

--- public/main.js
+++ public/main.js
@@ -1,11 +1,11 @@
 function showProtocol(){ 
-    document.querySelector(".portal-rastreabilidade").style.background = "#ffffff"; 
+    document.querySelector(".portal-rastreabilidade").style.background = 'none'; 
     document.querySelector(".background-corte").style.background = "none";  
-    document.querySelector(".footerbar").style.display = "none";
-    // document.querySelector(".titulo-header").style.display = "block"
-    // document.querySelector(".navbar-container").style.marginRight = "17vw"
-    document.getElementById('content').innerHTML = 
-  '<p> PROTOCOLO java script</p>';   
+    document.querySelector(".footer").style.display = "none"; ; 
+
+    document.getElementById('protocol-form-container').style.display = "flex";
+    document.getElementById('home').style.display = "none";
+     
 }
 
 
@@ -24,15 +24,7 @@ function showHome(){
     backgroundCorte.style.backgroundSize = "cover";
     backgroundCorte.style.backgroundPosition = "center"; 
 
-    document.querySelector(".footerbar").style.display = "flex";
-    // document.querySelector(".titulo-header").style.display = "none"
-    // document.querySelector(".navbar-container").style.marginRight = "0"     
-    document.getElementById('content').innerHTML = 
-    '<div class="home-header">'+
-        '<div class="home-header-content">'+
-        '<h1 class="portal">Portal</h1>'+
-        '<h2 class="rastreabilidade">Rastreabilidade</h2>'+
-        '</div>'+
-    '</div>;'   
+    document.querySelector(".footer").style.display = "flex";        
+       
 }
 
